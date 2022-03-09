534e7334d8d9d90c4aaaee11f624a070c8be06c1
Shegeda <shegedasasha@gmail.com>
2022-03-09 19:13:44 +0200

    v1.07 deploy

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import axios from "axios";
 
 import Card from "../components/Card";
-// import AppContext from "../context";
+// import AppContext from "../context";n
 
 
 function Orders() {
